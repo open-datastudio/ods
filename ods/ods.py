c90c05a693b6014d9732e4571975b8e2b7d6a7a0
Lee moon soo <leemoonsoo@gmail.com>
2020-08-30 10:48:45 -0700

    option to automatically configure delta lake

--- ods/ods.py
+++ ods/ods.py
@@ -208,7 +208,6 @@ def init(ske=None):
     __singleton["instance"] = Ods(ske=ske)
     return __singleton["instance"]
 
-def spark(name, spark_conf=None, chisel_path=None, worker_num=1, worker_type="standard-4", worker_isolation="dedicated", delta=False, aws=False):
+def spark(name, spark_conf=None, chisel_path=None, worker_num=1, worker_type="standard-4", worker_isolation="dedicated", delta=False, aws=True):
     cluster = SparkCluster(__singleton["instance"], name, spark_conf=spark_conf, worker_num=worker_num, worker_type=worker_type, worker_isolation=worker_isolation, delta=delta, aws=aws)
     return cluster
-
