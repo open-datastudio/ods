5ff54888e421818a4693bb4c859e0562181d145f
Lee moon soo <leemoonsoo@gmail.com>
2020-09-08 16:27:57 -0700

    remove unnecessary code

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ods",
-    version="0.0.5",
+    version="0.0.6",
     license='MIT',
     author="Open Data Studio",
     author_email="moon@staroid.com",
